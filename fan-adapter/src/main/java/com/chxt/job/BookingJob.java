1486fa82e1edfa2c685fee98ed1e5544961cca1c
chenxintong <chenxintong@pxb7.com>
2026-05-16 16:17:53 +0800

    source

--- fan-adapter/src/main/java/com/chxt/job/BookingJob.java
+++ fan-adapter/src/main/java/com/chxt/job/BookingJob.java
@@ -9,13 +9,13 @@ import com.chxt.notice.TennisNoticeService;
 import jakarta.annotation.Resource;
 
 @Component
-@ConditionalOnProperty(name = "job.tennis.enabled", havingValue = "true", matchIfMissing = false)
-public class TennisJob {
+@ConditionalOnProperty(name = "job.booking.enabled", havingValue = "true", matchIfMissing = false)
+public class BookingJob {
 
     @Resource
     private TennisNoticeService tennisNoticeService;
 
-    @Scheduled(cron = "0 */5 * * * *") // 每5分钟执行一次
+    @Scheduled(cron = "${job.booking.tennis.cron}") // 每5分钟执行一次
     public void scheduledTouch() {
         tennisNoticeService.touch();
     }
